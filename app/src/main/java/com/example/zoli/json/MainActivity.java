d2ac14ef13dbdf6911e5fd99e30e7f0d2f96d9af
regeim <forsimi@gmail.com>
2015-02-27 16:38:52 +0100

    27.02.2015

--- app/src/main/java/com/example/zoli/json/MainActivity.java
+++ app/src/main/java/com/example/zoli/json/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import org.apache.http.client.HttpClient;
@@ -18,24 +21,43 @@ import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends ActionBarActivity {
     private HttpConnection mainHttp;
+    String selected_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         Button get_btn=(Button)findViewById(R.id.get_button_id);
 
+        final Spinner spinner = (Spinner) findViewById(R.id.realmspinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.realms,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selected_item=spinner.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     public void buttonpressed (View v) throws JSONException, IOException {
-        String url="http://maps.googleapis.com/maps/api/geocode/json?address=Budapest%20Astoria&sensor=false";
+        String url="http://www.androidbegin.com/tutorial/jsonparsetutorial.txt";
         String holder;
-//        Toast.makeText(getApplicationContext(), "hello world",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), selected_item,Toast.LENGTH_SHORT).show();
         mainHttp=new HttpConnection(url,getApplicationContext());
         mainHttp.OpenStream();
         mainHttp.getBytes();
+        if (mainHttp.getBuf()!=null){
         holder = new String(mainHttp.getBuf(),"UTF-8");
-        JSONObject root=new JSONObject(holder);
+        JSONObject root=new JSONObject(holder);}
 
         mainHttp.CloseStream();
 
