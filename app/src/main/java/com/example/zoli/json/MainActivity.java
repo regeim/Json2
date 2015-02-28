6b17a0b33809fdea1aea6ec30003d53c0da87943
Zoli <cherrywidget@gmail.com>
2015-02-28 05:12:06 +0100

    28.02.2015.

--- app/src/main/java/com/example/zoli/json/MainActivity.java
+++ app/src/main/java/com/example/zoli/json/MainActivity.java
@@ -22,6 +22,8 @@ import java.io.UnsupportedEncodingException;
 public class MainActivity extends ActionBarActivity {
     private HttpConnection mainHttp;
     String selected_item;
+    String url="http://www.androidbegin.com/tutorial/jsonparsetutorial.txt";
+    String holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +33,20 @@ public class MainActivity extends ActionBarActivity {
 
         Button get_btn=(Button)findViewById(R.id.get_button_id);
 
-        final Spinner spinner = (Spinner) findViewById(R.id.realmspinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.realms,android.R.layout.simple_spinner_item);
+        final Spinner spinner = (Spinner) findViewById(R.id.realm_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.regions,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selected_item=spinner.getSelectedItem().toString();
+                try {
+                    spinner_item_selected(view,spinner);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -48,21 +56,31 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
-    public void buttonpressed (View v) throws JSONException, IOException {
-        String url="http://www.androidbegin.com/tutorial/jsonparsetutorial.txt";
-        String holder;
-        Toast.makeText(getApplicationContext(), selected_item,Toast.LENGTH_SHORT).show();
-        mainHttp=new HttpConnection(url,getApplicationContext());
-        mainHttp.OpenStream();
-        mainHttp.getBytes();
-        if (mainHttp.getBuf()!=null){
-        holder = new String(mainHttp.getBuf(),"UTF-8");
-        JSONObject root=new JSONObject(holder);}
 
-        mainHttp.CloseStream();
+    public void spinner_item_selected (View v, Spinner sp)throws JSONException, IOException{
+        switch (v.getId()){
+            case R.id.realm_spinner:
+                selected_item=sp.getSelectedItem().toString();
+                mainHttp=new HttpConnection(url,getApplicationContext());
+                mainHttp.OpenStream();
+                mainHttp.getBytes();
+                if (mainHttp.getBuf()!=null){
+                    holder = new String(mainHttp.getBuf(),"UTF-8");
+                }
+                mainHttp.CloseStream();
+                break;
+        }
 
     }
+    public void button_pressed (View v) throws JSONException, IOException {
 
+//        Toast.makeText(getApplicationContext(), selected_item,Toast.LENGTH_SHORT).show();
+
+        JSONObject root=new JSONObject(holder);
+
+
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/example/zoli/json/Realm.java
@@ -0,0 +1,16 @@
+package com.example.zoli.json;
+
+/**
+ * Created by Zoli on 2015.02.28..
+ */
+public class Realm {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
